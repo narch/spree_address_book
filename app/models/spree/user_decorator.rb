183471010c3995e2b2a39041c00517930d1b8cf0
Nathan Williams <nathan@nathan.la>
2019-12-11 15:02:50 -0800

    add priority sorting order to addresses

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,3 +1,3 @@
 Spree.user_class.class_eval do
-  has_many :addresses, -> { where(deleted_at: nil).order('created_at') }, class_name: 'Spree::Address'
+  has_many :addresses, -> { where(deleted_at: nil).order('priority desc, created_at asc') }, class_name: 'Spree::Address'
 end
